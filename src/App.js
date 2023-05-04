978fd8a872b611ada7c80057a34d07eded25af9d
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-05-04 20:54:35 +0200

    lab7

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from "./components/QuizComponent";
+function App() {
+  return (
+    <div>
+        <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
