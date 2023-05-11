a54a7bc6ab96b2124fa9e8c31e0698164159c07a
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-05-11 22:06:45 +0200

    lab8

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Posts from "./components/posts";
+
+function App() {
+  return (
+      <main className="container">
+        <Posts/>
+      </main>
+  );
+}
+
+export default App;
