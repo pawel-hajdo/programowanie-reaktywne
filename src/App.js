37c4a70b767a03a2619c6ca9b8091d153c2190f2
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-06-01 21:42:25 +0200

    lab9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-
 import './App.css';
-import Posts from "./components/posts";
+import Navbar from "./components/navbar";
+import { Outlet } from "react-router-dom";
+
 
 function App() {
-  return (
-      <main className="container">
-        <Posts/>
-      </main>
-  );
+    return (
+        <div className="container-fluid">
+            <Navbar />
+            <div className="container">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import NotFound from "./components/notFound";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}>
+                    <Route path="home" element={<Home/>}/>
+                    <Route path="posts" element={<Posts/>}/>
+                    <Route
+                        path="*"
+                        element={
+                            <NotFound/>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
